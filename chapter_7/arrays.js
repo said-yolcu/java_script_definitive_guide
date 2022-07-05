1a87d67df125c9d88ed179e1a834cec66e5c7602
said-yolcu <saidyolcu.saidyolcu@gmail.com>
2022-07-05 18:22:08 +0300

    Up to 7.8

--- chapter_7/arrays.js
+++ chapter_7/arrays.js
@@ -34,3 +34,70 @@ console.log(b)
 Array.of(10) // => [10] only this is different than constructor 
 let copy_of_c= Array.from(c)
 console.log(copy_of_c)
+
+// a more efficient mapping than map()
+let mapArray= Array.from(c, elem => 2*elem)
+console.log(mapArray)
+
+// reading and writing array elements
+let arr= [0,1,2]
+arr[3]= 13
+console.log(arr[3])
+console.log(arr['3'])
+
+arr[5]= 34 // now index 4 is empty. We created a sparse array
+console.log(arr)
+// below creates a property '-1.23', does not change the length
+arr[-1.23]= 34; 
+// below creates an index, changes the length
+arr[23]= '23 times masallah'
+console.log(arr.length)
+console.log(arr)
+// no 'out of bounds' error. Only returns undefined
+console.log(arr)
+delete arr[23] // deleting the last element doesn't alter the length
+console.log(arr)
+
+// add to the end
+arr.push('end')
+// add to the beginning
+arr.unshift('beginning')
+// pop() , removes and returns the last element, decrements the length
+arr.pop()
+// shift(), removes and returns the first element, decrements the length
+arr.shift()
+
+arr[10]= undefined
+console.log(arr)
+
+let non_empty=[]
+for(let [index, elem] of arr.entries()){
+    // pass empty and undefined elements, but not 0's
+    // turns out the commented out part is unnecessary
+    // empty elements evaluates to undefined
+    // second check is enough
+    /*(index in arr) &&*/ (arr[index] !== undefined) && 
+        non_empty.push(elem) 
+}
+console.log(non_empty)
+
+for(let p in arr){
+    (arr[p] !== undefined) && console.log(`${p} : ${arr[p]}`)
+}
+
+let for_each_arr= []
+arr.forEach((elem, index) => {
+    for_each_arr+=`${index}: ${elem}----`
+})
+console.log('for each: ', for_each_arr)
+
+let vowel_count=0;
+let vowels= ''
+let phrase= 'Ey Enver mazimin şehit yiğidi'
+for(let letter of phrase){
+    if(/[aeıioöuü]/.test(letter.toLowerCase())){
+        vowels+= letter
+        vowel_count++
+    }
+}
+console.log(`there are ${vowel_count} vowels in phrase. Vowels are ${vowels}`)
